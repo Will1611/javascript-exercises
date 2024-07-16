5841c4434d8ccfee10668e0686c69d576127cde7
Will <willpearson9120@gmail.com>
2024-07-16 14:31:45 +0100

    Final commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,3 +1,7 @@
+// .split("") splits string into array
+// .reverse() reverses the array indexes
+// .join("") joins the reversed array indexes back into a new string
+
 const reverseString = function (string) {
   return string.split("").reverse().join("");
 };
