fd576d1505abb7369d14fbe9001e1a8e6374a82a
Will <willpearson9120@gmail.com>
2024-07-16 11:46:15 +0100

    Final commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    str += string;
+  }
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    return str;
+  }
 };
 
 // Do not edit below this line
